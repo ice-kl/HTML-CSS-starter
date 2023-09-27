de1f940d5e2be48f0a9520df69b0285095323c1b
ice-kl <123858675+ice-kl@users.noreply.github.com>
2023-09-27 17:49:24 +0700

    Add files via upload

---
+++ ES6/arrowFunction.js
@@ -0,0 +1,16 @@
+/* Function แบบเดิม
+
+function fullname(fname,lname){
+    return fname+lname
+}
+
+*/
+
+//Arrow function
+
+fullname = (fname,lname) => fname+lname
+console.log(fullname('Joody','Coo'));
+
+
+setAge = (age) => "Age = "+age
+console.log(setAge(20));
