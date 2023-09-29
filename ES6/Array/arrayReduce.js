1c06ec44a7313d24c9c113bae967e0974541544d
ice-kl <123858675+ice-kl@users.noreply.github.com>
2023-09-29 14:15:07 +0700

    Add files via upload

--- ES6/Array/arrayReduce.js
+++ ES6/Array/arrayReduce.js
@@ -5,7 +5,22 @@
 
 const data = [10,20,30,40]
 
-data.reduce((value, e) => {        // value = 0 
+const summation = data.reduce((value, e) => {        // value = 0 
     const total = e + value        // total = 10 + 0 = 10  loop ถัดไป total จะถูกกำหนดค่าไว้ใน value
     return total
 },0)
+
+console.log(summation);
+// value = 0 10 30 60 100
+// (0), (10+0), (20+10), (30+30), (40+60)
+
+
+// Object
+// ต้องการหาผลรวมว่า 3 ชิ้นนี้ราคาเท่าไหร่
+const cart = [
+    {name:'Bag', price:500},
+    {name:'Book', price:900},
+    {name:'Camera', price:5000}
+]
+const totalPayment = cart.reduce((value, e) => e.price + value, 0)
+console.log(`ลูกค้าต้องชำระเงิน = ${totalPayment} บาท `);
